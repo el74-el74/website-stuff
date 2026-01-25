ab60b14c45a1c7ca75884092289b5dbbcee5d098
el74-el74 <eleanor.c.r123@btinternet.com>
2026-01-25 16:04:34 +0000

    seperated css, js and html files

---
+++ My Website/script.js
@@ -0,0 +1,39 @@
+/* ==== Javascript ==== */
+
+
+/* ==== clock ==== */
+function updateClock() {
+    const now = new Date();  
+    const time = now.toLocaleTimeString([], {
+        hour: '2-digit',
+        minute: '2-digit'
+    });
+    
+    document.getElementById("clock").textContent = time;
+}
+
+updateClock(); // instant display of clock
+
+const now = new Date();
+const delay = 60000 - (now.getSeconds() * 1000);  // getting seconds then converting to milliseconds, then calculating how long to wait before updating
+
+
+setTimeout(() => {  // runs once after a delay, "=>" tells code to run later
+updateClock(); 
+    setInterval(updateClock, 60000); // updates every minute
+}, delay);
+
+/* ==== date ==== */
+function updateDate() {
+    const day = new Date(); // gives current date and time
+    const date = day.toLocaleDateString([], {  // formats date nicely
+        weekday: 'short', // short day e.g. Mon
+        month: 'short', // short month e.g. Sep
+        day: 'numeric' // numeric date e.g. 14
+    }).toLowerCase();
+    
+document.getElementById("date").textContent = date;
+
+}
+updateDate();
+setInterval(updateDate, 86400000);
