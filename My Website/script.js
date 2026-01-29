6fc6b98393cc25951dffd1176776c9c3f5ecd5f6
el74-el74 <eleanor.c.r123@btinternet.com>
2026-01-29 12:23:24 +0000

    tried to do some js but lwk broke everything lol

--- My Website/script.js
+++ My Website/script.js
@@ -1,5 +1,7 @@
 /* ==== Javascript ==== */
 
+const path = require('path');
+
 
 /* ==== clock ==== */
 function updateClock() {
@@ -37,3 +39,12 @@ document.getElementById("date").textContent = date;
 }
 updateDate();
 setInterval(updateDate, 86400000);
+
+
+/* ==== latest page added ==== */
+function getNewPage() {
+    var fs = require('fs');
+    path.basename('/My Website/finished pages/finishedpage.html', '.html');
+}
+
+getNewPage();
